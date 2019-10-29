611e87a11eef28be687b40db0e9081284e2e4fb5
Jeffrey Lovitz <jeffrey.lovitz@gmail.com>
2019-10-29 10:30:23 -0400

    Update ExecutionPlan constructors

--- src/commands/cmd_explain.c
+++ src/commands/cmd_explain.c
@@ -51,7 +51,7 @@ void Graph_Explain(void *args) {
 	Graph_AcquireReadLock(gc->g);
 	lock_acquired = true;
 
-	plan = NewExecutionPlan(ctx, gc, NULL);
+	plan = NewExecutionPlan(NULL);
 	if(plan) ExecutionPlan_Print(plan, ctx);
 
 cleanup:
